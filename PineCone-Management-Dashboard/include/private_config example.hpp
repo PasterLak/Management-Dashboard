7fd30a271e96a27f8f4c38dad6785241738d4f89
Vladislav Bezborodyi <paster.lak@gmail.com>
2026-03-14 09:33:10 +0100

    Update private_config example.hpp

--- PineCone-Management-Dashboard/include/private_config example.hpp
+++ PineCone-Management-Dashboard/include/private_config example.hpp
@@ -5,5 +5,5 @@
 // COPY THIS FILE AND RENAME -> private_config.hpp
 
 #define PRIVATE_WIFI_SSID "my_wlan_name"
-#define PRIVATE_WIFI_PASSWORD "secret_password"
-#define PRIVATE_DASHBOARD_SERVER_IP "192.168.0.1"
+#define PRIVATE_WIFI_PASSWORD "my_secret_password"
+#define PRIVATE_DASHBOARD_SERVER_IP "127.0.0.1"
